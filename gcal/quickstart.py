813623c2bd0578a6490ff0042df5e0c47cc23def
dalbodeule <11470513+dalbodeule@users.noreply.github.com>
2024-03-01 22:15:19 +0900

    asdf

--- gcal/quickstart.py
+++ gcal/quickstart.py
@@ -30,17 +30,23 @@ def main():
         with open('token.pickle', 'rb') as token:
             creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
+    
     if not creds or not creds.valid:
+        
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=80)
+            creds = flow.run_local_server(host='localhost',
+                port=0, 
+                authorization_prompt_message='Please visit this URL: {url}', 
+                success_message='The auth flow is complete; you may close this window.',
+                open_browser=True)
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
-
+    print(creds.refresh_token)
     service = build('calendar', 'v3', credentials=creds)
 
     # Call the Calendar API
